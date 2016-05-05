c7e935d2f05c1052476fab6b9932570ea0a99df2
esegal8 <esegal8@students.d125.org>
2016-05-05 10:32:14 -0500

    hi :wq

--- building.java
+++ building.java
@@ -3,14 +3,18 @@ public abstract class Building extends CookieGame
     private int gain;
     private int number;
     private double multiplier;
-    public Building()
+    public Building(int g, double m)
     {
-        
+        gain = g;
+        multiplier = m;
+        number = 0;
     }
     public void buy()
     {
-        
         number++;
     }
-    
+    public int cookies()
+    {
+        
+    }
 }
