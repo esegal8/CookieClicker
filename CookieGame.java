9675d4671231a7390f399d2ccd26717a79b6998f
esegal8 <esegal8@students.d125.org>
2016-05-06 10:30:27 -0500

    die bluej

---
+++ CookieGame.java
@@ -0,0 +1,15 @@
+package cookieclickerproject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author esegal8
+ */
+public class CookieGame extends CookieClickerProject{
+    
+}
