a297276b1c73fb507b8e9626b57b45deb144964f
esegal8 <esegal8@students.d125.org>
2016-05-11 10:37:10 -0500

    haha bulej

--- CookieClickerProject/src/cookieclickerproject/AchievementList.java
+++ CookieClickerProject/src/cookieclickerproject/AchievementList.java
@@ -39,50 +39,59 @@ public class AchievementList {
     }
     public void CheckA2(CookieGame hi)
     {
-        if ((hi.getTotalCookies()>=10000) && !(pie.get(0).getBool()))
+        if ((hi.getTotalCookies()>=10000) && !(pie.get(1).getBool()))
             pie.get(1
             ).setBool(true);
     }
     public void CheckA3(CookieGame hi)
     {
-        if (hi.getBakeryName().equals("Elliot"))
+        if ((hi.getBakeryName().equals("Elliot")) && !(pie.get(2).getBool()))
             pie.get(2).setBool(true);
     }
     public void CheckA4(CookieGame hi)
     {
-        
+        if ((Building.getNumBuildings()>=100) && !(pie.get(3).getBool()))
+            pie.get(3).setBool(true);
     }
     public void CheckA5(CookieGame hi)
     {
-        
+        if ((hi.getClicksPerSecond()>=10) && !(pie.get(4).getBool()))
+            pie.get(4).setBool(true);
     }
     public void CheckA6(CookieGame hi)
     {
-        
+        if ((hi.getTotalClicks()>=5000) && !(pie.get(5).getBool()))
+            pie.get(5).setBool(true);
     }
     public void CheckA7(CookieGame hi)
     {
-        
+        if ((hi.getTotalClicks()>=9001) && !(pie.get(6).getBool()))
+            pie.get(6).setBool(true);
     }
     public void CheckA8(CookieGame hi)
     {
-        
+        if ((hi.getTotalUpgrades()>=1) && !(pie.get(7).getBool()))
+            pie.get(7).setBool(true);
     }
     public void CheckA9(CookieGame hi)
     {
-        
+        if ((hi.getTotalUpgrades()>=3) && !(pie.get(8).getBool()))
+            pie.get(8).setBool(true);
     }
     public void CheckA10(CookieGame hi)
     {
-        
+        if ((hi.getTotalUpgrades()>=10) && !(pie.get(9).getBool()))
+            pie.get(9).setBool(true);
     }
-    public void CheckA11(CookieGame hi)
+    public void CheckA11(Building cookieLoop)
     {
-        
+        if ((cookieLoop.getNumBuildings()>=100) && !(pie.get(10).getBool()))
+            pie.get(10).setBool(true);
     }
-    public void CheckA12(CookieGame hi)
+    public void CheckA12(Building cookieTree)//mine
     {
-        
+        if ((cookieTree.getNumBuildings()>=80) && !(pie.get(11).getBool()))
+            pie.get(11).setBool(true);
     }
     public void CheckA13(CookieGame hi)
     {
@@ -108,5 +117,4 @@ public class AchievementList {
     {
         
     }
-    
 }
